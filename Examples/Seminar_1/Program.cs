7497928303199a38b1de42ec465370fab2b82fc8
Pavel Peredrij <peredrij.pavel19@gmail.com>
2023-09-18 20:40:42 +0300

    added Task1

---
+++ Examples/Seminar_1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Task 1. Напишите программу, которая на вход принимает число и выдает его квадрат
+Console.WriteLine("Input number: ");
+string numberString = Console.ReadLine();
+int numberInt = Convert.ToInt32(numberString);
+int res = numberInt * numberInt;
+Console.WriteLine(res);
