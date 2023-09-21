c5de62c3d1daa0afa8045d13389ad1b8b236646e
Pavel Peredrij <peredrij.pavel19@gmail.com>
2023-09-21 22:31:00 +0300

    Added seminar_2 tasks and solution with diff

--- Examples/Seminar_2/Program.cs
+++ Examples/Seminar_2/Program.cs
@@ -135,9 +135,9 @@ class Program
 */
 
 Console.WriteLine("Input first number");
-int num1 = Convert.ToInt32(Console.ReadLine());
+double num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input second number");
-int num2 = Convert.ToInt32(Console.ReadLine());
+double num2 = Convert.ToInt32(Console.ReadLine());
 
 if(num1 == num2*num2 || num2 == num1*num1)
 {
